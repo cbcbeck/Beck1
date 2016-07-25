ace205353119d7fb99d94c363f086cce2dbf27f4
cbcbeck <Hebrews43@gmail.com>
2016-07-25 06:12:23 -0400

    first commit

---
+++ Beck1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beck1
+{
+    class StringOperations
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a phrase to reverse: "); 
+            string phrase = Console.ReadLine(); 
+            StringOperations.ReverseWordsInSentence(phrase);
+            Console.ReadKey();
+        } 
+                        
+        public static void ReverseWordsInSentence(string phrase)
+        {
+            string reversed = string.Empty;
+            string[] words = phrase.Split(' '); 
+            Array.Reverse(words); 
+            reversed = string.Join(" ", words); 
+            Console.WriteLine("The reversed phrase is: " + reversed); 
+        }
+
+
+
+
+
+    }
+}
